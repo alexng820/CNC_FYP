f224b8de76d565ced830717f95b63129f32db7a5
Ash Chan <ashchan1122@gmail.com>
2017-01-14 05:29:08 +0800

    Add login functions and activity for user login

---
+++ app/src/main/java/fyp/cnc/cnc_fyp/activity/LoginActivity.java
@@ -0,0 +1,176 @@
+package fyp.cnc.cnc_fyp.activity;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request.Method;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import fyp.cnc.cnc_fyp.R;
+import fyp.cnc.cnc_fyp.app.AppConfig;
+import fyp.cnc.cnc_fyp.app.AppController;
+import fyp.cnc.cnc_fyp.helper.SQLiteHandler;
+import fyp.cnc.cnc_fyp.helper.SessionManager;
+
+public class LoginActivity extends Activity {
+    private static final String TAG = RegisterActivity.class.getSimpleName();
+    private Button buttonLogin;
+    private Button buttonLinkToRegister;
+    private EditText inputUserID;
+    private EditText inputUserPass;
+    private ProgressDialog progressDialog;
+    private SessionManager session;
+    private SQLiteHandler db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        inputUserID = (EditText) findViewById(R.id.userID);
+        inputUserPass = (EditText) findViewById(R.id.userPass);
+        buttonLogin = (Button) findViewById(R.id.button_Login);
+        buttonLinkToRegister = (Button) findViewById(R.id.button_To_Register);
+
+        //Progress dialog
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+
+        //SQLite database handler
+        db = new SQLiteHandler(getApplicationContext());
+
+        //Session manager
+        session = new SessionManager(getApplicationContext());
+
+        //Check if user is already logged in or not
+        if (session.isLoggedIn()) {
+            //User is already logged in
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        //Login button onClick event
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String userID = inputUserID.getText().toString().trim();
+                String userPass = inputUserPass.getText().toString().trim();
+
+                //Check for empty data in form
+                if (!userID.isEmpty() && !userPass.isEmpty()) {
+                    //Login
+                    checkLogin(userID, userPass);
+                } else {
+                    //Required credentials are missing
+                    Toast.makeText(getApplicationContext(), "Please enter your login credentials.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        //Link to Register
+        buttonLinkToRegister.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+
+    //Verify login details
+    private void checkLogin(final String userID, final String userPass) {
+        //Tag used to cancel the request
+        String tag_string_req = "req_login";
+
+        progressDialog.setMessage("Logging in...");
+        showDialog();
+
+        StringRequest strRequest = new StringRequest(Method.POST, AppConfig.URL_LOGIN, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d(TAG, "Login Response: " + response.toString());
+                hideDialog();
+
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    boolean error = jsonObject.getBoolean("error");
+
+                    //Check for error
+                    if (!error) {
+                        //Log in successful
+                        session.setLogin(true);
+
+                        //Store the ser in SQLite
+                        JSONObject user = jsonObject.getJSONObject("user");
+                        String userID = user.getString("userID");
+                        String userRole = user.getString("userRole");
+                        String userStatus = user.getString("userStatus");
+
+                        //Insert row to user table
+                        db.addUser(userID, userRole, userStatus);
+
+                        //Launch main activity
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        //Get error
+                        String errorMsg = jsonObject.getString("error_msg");
+                        Toast.makeText(getApplicationContext(), errorMsg, Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    //JSON error
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "JSON Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "Login Error: " + error.getMessage());
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                hideDialog();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() {
+                //Post parameters to login URL
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("userID", userID);
+                params.put("userPass", userPass);
+
+                return params;
+            }
+        };
+
+        //Adding request to request queue
+        AppController.getInstance().addToRequestQueue(strRequest, tag_string_req);
+    }
+
+    private void showDialog() {
+        if (!progressDialog.isShowing()) {
+            progressDialog.show();
+        }
+    }
+
+    private void hideDialog() {
+        if (progressDialog.isShowing()) {
+            progressDialog.dismiss();
+        }
+    }
+}
---
+++ app/src/main/java/fyp/cnc/cnc_fyp/helper/SQLiteHandler.java
@@ -0,0 +1,104 @@
+package fyp.cnc.cnc_fyp.helper;
+
+//Class for handling database operations
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.HashMap;
+
+public class SQLiteHandler extends SQLiteOpenHelper {
+    private static final String TAG = SQLiteHandler.class.getSimpleName();
+
+    //All Static variables
+    //Database version
+    private static final int DATABASE_VERSION = 1;
+
+    //Database name
+    private static final String DATABASE_NAME = "cncfyp";
+
+    //Login table name
+    private static final String TABLE_USER = "user";
+
+    //login table columns names
+    private static final String KEY_ID = "user_id";
+    private static final String KEY_ROLE = "role";
+    private static final String KEY_STATUS = "status";
+
+    public SQLiteHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    //Createing tables
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_LOGIN_TABLE = "CREATE TABLE " + TABLE_USER + "(" + KEY_ID + " VARCHAR(25) PRIMARY KEY, " + KEY_ROLE + " VARCHAR(25), " + KEY_STATUS + " VARCHAR(25))";
+        db.execSQL(CREATE_LOGIN_TABLE);;
+
+        Log.d(TAG, "Database table created.");
+    }
+
+    //Upgrading database
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //Drop table if exist
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_USER);
+
+        //Create table
+        onCreate(db);
+    }
+
+    //Storing user details in database
+    public void addUser(String userID, String userRole, String userStatus) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_ID, userID);
+        values.put(KEY_ROLE, userRole);
+        values.put(KEY_STATUS, userStatus);
+
+        //Inserting row
+        long id = db.insert(TABLE_USER, null, values);
+        db.close();
+
+        Log.d(TAG, "New user inserted into sqlite: " + id);
+    }
+
+    //Getting user data from database
+    public HashMap<String, String> getUserDetails() {
+        HashMap<String, String> user = new HashMap<String, String>();
+        String selectQuery = "SELECT * FROM " + TABLE_USER;
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        //Move to first row
+        cursor.moveToFirst();
+        if (cursor.getCount() > 0) {
+            user.put("userID", cursor.getString(1));
+            user.put("userRole", cursor.getString(2));
+            user.put("userStatus", cursor.getString(3));
+        }
+        cursor.close();
+        db.close();
+
+        Log.d(TAG, "Fetching user from sqlite: " + user.toString());
+
+        //Return user
+        return user;
+    }
+
+    //Delete all rows
+    public void deleteUsers() {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        db.delete(TABLE_USER, null, null);
+        db.close();
+
+        Log.d(TAG, "Deleted all user data from sqlite");
+    }
+}
