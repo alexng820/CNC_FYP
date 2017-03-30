8a265658bb18d36ac75ae6cc53ed353f5404fc83
Ash Chan <ashchan1122@gmail.com>
2017-03-30 16:48:49 +0800

    Fix an issue that logout will cause app force close

--- app/src/main/java/fyp/cnc/cnc_fyp/activity/NavBarTemplate.java
+++ app/src/main/java/fyp/cnc/cnc_fyp/activity/NavBarTemplate.java
@@ -2,27 +2,22 @@ package fyp.cnc.cnc_fyp.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 import fyp.cnc.cnc_fyp.R;
 import fyp.cnc.cnc_fyp.helper.SQLiteHandler;
 import fyp.cnc.cnc_fyp.helper.SessionManager;
 
-public class NavBarTemplate extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
-
+public class NavBarTemplate extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private SQLiteHandler db;
     private SessionManager session;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,16 +25,18 @@ public class NavBarTemplate extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
         db = new SQLiteHandler(getApplicationContext());
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        session = new SessionManager(getApplicationContext());
+        if (!session.isLoggedIn()) {
+            logoutUser();
+        }
     }
 
     @Override
@@ -51,8 +48,6 @@ public class NavBarTemplate extends AppCompatActivity
             super.onBackPressed();
         }
     }
-
-
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -73,6 +68,7 @@ public class NavBarTemplate extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
     private void logoutUser() {
         session.setLogin(false);
 
